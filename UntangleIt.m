77c2232bbeb605afbe3a800c9492fbca267ee24f
WeGlove <tobias.jungbluth.tj@gmail.com>
2019-11-05 18:07:16 +0100

    Added b)

--- UntangleIt.m
+++ UntangleIt.m
@@ -1,27 +1,30 @@
 n = 20;
-X = (rand(n,1)+1)*1;
-Y = (rand(n,1)+1)*1;
+X = (rand(n,1)+1)*100;
+Y = (rand(n,1)+1)*100;
 k=500;
-delay = 0;
-%TODO How to get arbitrary X and Y
+delay = 0.5;
 
+clf("reset")
 hold on;
 
 show(X,Y,n,k,delay);
 
+%Plots multiple iterations of X and Y
 function [] = show(X, Y, verticies, amount, delay)
     for i = 1:amount
         pause(delay);
-        [X, Y] = iterate(X,Y,verticies);
+        [X, Y] = shift(X,Y,verticies);
         plotThis(X,Y);  
     end
 end
 
+%Plots the Polygon
 function  [] = plotThis(X, Y)
     pgon = polyshape(X,Y);
     plot(pgon);
 end
 
+%Does One iteration step of 1) a)
 function [X, Y] = iterate(XIn, YIn, n)
     X = zeros(n,0);
     Y = zeros(n,0);
@@ -35,5 +38,21 @@ function [X, Y] = iterate(XIn, YIn, n)
             Y(i) = (YIn(i) + YIn(1))*0.5;
         end
         
+    end
+end
+
+%Does the shifting and normalization in b)
+function [X,Y] = shift(XIn, YIn,n)
+    [X,Y] = iterate(XIn,YIn,n);
+    meanX = mean(X);
+    meanY = mean(Y);
+    for i = 1:n
+        X(i) = X(i) - meanX;
+        Y(i) = Y(i) - meanY;
+    end
+    
+    for i = 1:n
+        X(i) = X(i) / norm(X);
+        Y(i) = Y(i) / norm(Y);
     end
 end
